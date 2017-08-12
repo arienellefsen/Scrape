af1987beda47040b485517f6ec8ac036169a47ac
Aladim2017@ <ariene_silva@hotmail.com>
2017-08-12 13:24:58 -0400

    adding moongose db comnnection from heroku

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set("view engine", "handlebars");
 
 
 // Database configuration with mongoose
-mongoose.connect("mongodb://heroku_v7m5qfq9:jrqit7h9h4o8g5ls2hu0k2hl5t@ds153659.mlab.com:53659/heroku_v7m5qfq9");
+mongoose.connect("mongodb://heroku_t5wpqwl8:ctpgelbtcm86l2vuut6040o7q2@ds145639.mlab.com:45639/heroku_t5wpqwl8");
 var db = mongoose.connection;
 
 // Show any mongoose errors
